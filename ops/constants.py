558e46a3dbd3f780ea23ec220b3ec21b6687b8e5
vmirly <mirjalil@msu.edu>
2019-12-08 21:45:51 -0500

    Fixed the typos in lossfn names

--- ops/constants.py
+++ ops/constants.py
@@ -3,13 +3,13 @@ from losses import gan_losses
 
 
 GAN_LOSS = {
-    'vanilla': (gan_losses.vanilla_gan_loss_G,
-                gan_losses.vanialla_gan_loss_D_real,
-                gan_losses.vanialla_gan_loss_D_real),
+    'vanilla': (gan_losses.vanilla_gan_lossfn_G,
+                gan_losses.vanilla_gan_lossfn_D_real,
+                gan_losses.vanilla_gan_lossfn_D_fake),
 }
 
 RECONSTRUCTION_LOSS = {
-    'l1': img_losses.lossfn_l1,
-    'l2': img_losses.lossfn_l2,
+    'l1': img_losses.l1_lossfn,
+    'l2': img_losses.l2_lossfn,
     'ce': None
 }
