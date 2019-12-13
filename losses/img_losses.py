e259040e564c41a2c31a256c9ec87c963bd09a2e
vmirly <mirjalil@msu.edu>
2019-12-12 21:33:44 -0500

    Added reduction arg to l1_lossfn

--- losses/img_losses.py
+++ losses/img_losses.py
@@ -2,7 +2,7 @@ import torch.nn.functional as F
 
 
 def l1_lossfn(input, target):
-    return F.l1_loss(input, target)
+    return F.l1_loss(input, target, reduction='mean')
 
 
 def l2_lossfn(input, target):
