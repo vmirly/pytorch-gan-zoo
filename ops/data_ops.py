3d96431fc4e20b39c1eed327650114321349113e
vmirly <mirjalil@msu.edu>
2019-12-12 20:59:26 -0500

    Fixing the converion of a tensor to an image

--- ops/data_ops.py
+++ ops/data_ops.py
@@ -12,7 +12,7 @@ def convert_tensor2image(
 
     t_image = np.transpose(t_image, (1, 2, 0))
     if unnormalize:
-        t_image = 2.0*(t_image + torch.tensor([[[0.5, 0.5, 0.5]]]))
+        t_image = 0.5*t_image + torch.tensor([[[0.5, 0.5, 0.5]]])
 
     t_image = (t_image*255).numpy().astype('uint8')
 
--- ops/test_tensor2image.py
+++ ops/test_tensor2image.py
@@ -8,7 +8,7 @@ from ops import data_ops
 
 class TestImageConversion(unittest.TestCase):
 
-    def test_tensor2image_wihout_unnormalize(self):
+    def test_tensor2image(self):
         img = Image.open('data/1.jpg')
         print('Original size:', img.size)
 
@@ -21,5 +21,28 @@ class TestImageConversion(unittest.TestCase):
 
         print(img_converted.size)
         print(type(img_converted))
+
+        img.save('/tmp/image-original-1.png')
+        img_converted.save('/tmp/image-converted-1.png')
+
+        self.assertTrue(np.all(img_converted.size == img.size))
+
+    def test_tensor2image_with_normalize(self):
+
+        img = Image.open('data/1.jpg')
+
+        tsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=(0.5, 0.5, 0.5),
+                std=(0.5, 0.5, 0.5))
+        ])
+
+        t_array = tsfm(img)
+        img_converted = data_ops.convert_tensor2image(
+            t_array, unnormalize=True)
+
+        img.save('/tmp/image-original-2.png')
+        img_converted.save('/tmp/image-converted-2.png')
 
         self.assertTrue(np.all(img_converted.size == img.size))
