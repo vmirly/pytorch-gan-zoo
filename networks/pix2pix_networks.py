b74ef79b8fe1c5b131b5e853dfdd1ab7ed07fc03
vmirly <mirjalil@msu.edu>
2019-12-21 13:00:58 -0500

    Using ConvBlock

--- networks/pix2pix_networks.py
+++ networks/pix2pix_networks.py
@@ -1,6 +1,66 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+
+class ConvBlock(nn.Module):
+    """Convolutional Block with BN, ReLU."""
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            kernel_size,
+            name,
+            stride=1,
+            padding=0,
+            activation='relu',
+            bn=False,
+            transposed=False,
+            dropout=False):
+
+        super(ConvBlock, self).__init__()
+
+        self.main = nn.Sequential()
+
+        if not transposed:
+            self.main.add_module(
+                'conv-{}'.format(name),
+                nn.Conv2d(
+                    in_channels,
+                    out_channels,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    padding=padding,
+                    bias=False))
+        else:
+            self.main.add_module(
+                'tconv-{}'.format(name),
+                nn.ConvTranspose2d(
+                    in_channels,
+                    out_channels,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    padding=padding,
+                    output_padding=0))
+
+        if bn:
+            self.main.add_module(
+                'BN-'.format(name),
+                nn.BatchNorm2d(
+                    out_channels,
+                    affine=True,
+                    track_running_stats=True))
+
+        if activation == 'relu':
+            self.main.add_module(
+                'relu-{}'.format(name),
+                nn.ReLU(inplace=True))
+        elif activation == 'leaky_relu':
+            self.main.add_module(
+                'lrelu-{}'.format(name),
+                nn.LeakyReLU(inplace=True))
+
+    def forward(self, x):
+        return self.main(x)
 
 
 class Pix2PixGenerator(nn.Module):
@@ -12,131 +72,77 @@ class Pix2PixGenerator(nn.Module):
 
         super(Pix2PixGenerator, self).__init__()
 
-        # Contractive layers:
-        self.conv1a = nn.Conv2d(
+        self.conv_first = ConvBlock(
             in_channels=n_inp_channels, out_channels=n_filters,
-            kernel_size=3, stride=1, padding=1)
-        self.conv1b = nn.Conv2d(
-            in_channels=n_filters, out_channels=n_filters,
-            kernel_size=3, stride=1, padding=1)
+            kernel_size=1, name='first', stride=1, padding=0,
+            activation='relu', bn=False, dropout=False, transposed=False)
 
-        self.conv2a = nn.Conv2d(
+        # Contractive layers:
+        self.conv1 = ConvBlock(
             in_channels=n_filters, out_channels=n_filters*2,
-            kernel_size=3, stride=1, padding=1)
-        self.conv2b = nn.Conv2d(
-            in_channels=n_filters*2, out_channels=n_filters*2,
-            kernel_size=3, stride=1, padding=1)
+            kernel_size=3, name='c1', stride=2, padding=1,
+            activation='relu', bn=True, dropout=False, transposed=False)
 
-        self.conv3a = nn.Conv2d(
+        self.conv2 = ConvBlock(
             in_channels=n_filters*2, out_channels=n_filters*4,
-            kernel_size=3, stride=1, padding=1)
-        self.conv3b = nn.Conv2d(
-            in_channels=n_filters*4, out_channels=n_filters*4,
-            kernel_size=3, stride=1, padding=1)
+            kernel_size=3, name='c2', stride=2, padding=1,
+            activation='relu', bn=True, dropout=False, transposed=False)
 
-        self.conv4a = nn.Conv2d(
+        self.conv3 = ConvBlock(
             in_channels=n_filters*4, out_channels=n_filters*8,
-            kernel_size=3, stride=1, padding=1)
-        self.conv4b = nn.Conv2d(
-            in_channels=n_filters*8, out_channels=n_filters*8,
-            kernel_size=3, stride=1, padding=1)
+            kernel_size=3, name='c3', stride=2, padding=1,
+            activation='relu', bn=True, dropout=False, transposed=False)
 
-        self.conv5a = nn.Conv2d(
+        self.conv4 = ConvBlock(
             in_channels=n_filters*8, out_channels=n_filters*16,
-            kernel_size=3, stride=1, padding=1)
-        self.conv5b = nn.Conv2d(
-            in_channels=n_filters*16, out_channels=n_filters*16,
-            kernel_size=3, stride=1, padding=1)
+            kernel_size=3, name='c4', stride=2, padding=1,
+            activation='relu', bn=True, dropout=False, transposed=False)
 
         # Expansive layers:
-        self.t_conv6 = nn.ConvTranspose2d(
+        self.t_conv1 = ConvBlock(
             in_channels=n_filters*16, out_channels=n_filters*8,
-            kernel_size=4, stride=2, padding=1, output_padding=0)
-        self.conv6a = nn.Conv2d(
-            in_channels=n_filters*16, out_channels=n_filters*8,
-            kernel_size=3, stride=1, padding=1)
-        self.conv6b = nn.Conv2d(
-            in_channels=n_filters*8, out_channels=n_filters*8,
-            kernel_size=3, stride=1, padding=1)
+            kernel_size=4, name='e1', stride=2, padding=1,
+            activation='relu', bn=True, dropout=False, transposed=True)
 
-        self.t_conv7 = nn.ConvTranspose2d(
-            in_channels=n_filters*8, out_channels=n_filters*4,
-            kernel_size=4, stride=2, padding=1, output_padding=0)
-        self.conv7a = nn.Conv2d(
-            in_channels=n_filters*8, out_channels=n_filters*4,
-            kernel_size=3, stride=1, padding=1)
-        self.conv7b = nn.Conv2d(
-            in_channels=n_filters*4, out_channels=n_filters*4,
-            kernel_size=3, stride=1, padding=1)
+        self.t_conv2 = ConvBlock(
+            in_channels=n_filters*16, out_channels=n_filters*4,
+            kernel_size=4, name='e2', stride=2, padding=1,
+            activation='relu', bn=True, dropout=False, transposed=True)
 
-        self.t_conv8 = nn.ConvTranspose2d(
-            in_channels=n_filters*4, out_channels=n_filters*2,
-            kernel_size=4, stride=2, padding=1, output_padding=0)
-        self.conv8a = nn.Conv2d(
-            in_channels=n_filters*4, out_channels=n_filters*2,
-            kernel_size=3, stride=1, padding=1)
-        self.conv8b = nn.Conv2d(
-            in_channels=n_filters*2, out_channels=n_filters*2,
-            kernel_size=3, stride=1, padding=1)
+        self.t_conv3 = ConvBlock(
+            in_channels=n_filters*8, out_channels=n_filters*2,
+            kernel_size=4, name='e3', stride=2, padding=1,
+            activation='relu', bn=True, dropout=False, transposed=True)
 
-        self.t_conv9 = nn.ConvTranspose2d(
-            in_channels=n_filters*2, out_channels=n_filters,
-            kernel_size=4, stride=2, padding=1, output_padding=0)
-        self.conv9a = nn.Conv2d(
-            in_channels=n_filters*2, out_channels=n_filters,
-            kernel_size=3, stride=1, padding=1)
-        self.conv9b = nn.Conv2d(
-            in_channels=n_filters, out_channels=n_filters,
-            kernel_size=3, stride=1, padding=1)
+        self.t_conv4 = ConvBlock(
+            in_channels=n_filters*4, out_channels=n_filters,
+            kernel_size=4, name='e4', stride=2, padding=1,
+            activation='relu', bn=True, dropout=False, transposed=True)
 
         # Last conv layer avec 1x1 kernel
-        self.conv_out = nn.Conv2d(
+        self.conv_last = ConvBlock(
             in_channels=n_filters, out_channels=n_out_channels,
-            kernel_size=1, stride=1, padding=0)
+            kernel_size=1, name='last', stride=1, padding=0,
+            activation=None, bn=False, dropout=False, transposed=False)
 
     def forward(self, x):
         # Conctractive layers:
-        x1 = F.leaky_relu(self.conv1a(x))
-        x1 = F.leaky_relu(self.conv1b(x1))
-        x1pool = F.dropout(F.max_pool2d(x1, 2))
-
-        x2 = F.leaky_relu(self.conv2a(x1pool))
-        x2 = F.leaky_relu(self.conv2b(x2))
-        x2pool = F.dropout(F.max_pool2d(x2, 2))
-
-        x3 = F.leaky_relu(self.conv3a(x2pool))
-        x3 = F.leaky_relu(self.conv3b(x3))
-        x3pool = F.dropout(F.max_pool2d(x3, 2))
-
-        x4 = F.leaky_relu(self.conv4a(x3pool))
-        x4 = F.leaky_relu(self.conv4b(x4))
-        x4pool = F.dropout(F.max_pool2d(x4, 2))
-
-        x5 = F.leaky_relu(self.conv5a(x4pool))
-        x5 = F.leaky_relu(self.conv5b(x5))
+        x0 = self.conv_first(x)
+        x1 = self.conv1(x0)     # -> /2
+        x2 = self.conv2(x1)     # -> /4
+        x3 = self.conv3(x2)     # -> /8
+        x4 = self.conv4(x3)     # -> /16
 
         # Expansive layers:
-        x6 = F.leaky_relu(self.t_conv6(x5))
-        x6 = torch.cat([x6, x4], axis=1)
-        x6 = F.leaky_relu(self.conv6a(x6))
-        x6 = F.leaky_relu(self.conv6b(x6))
+        x5 = self.t_conv1(x4)   # -> /8
+        x5c = torch.cat([x5, x3], axis=1)
+        x6 = self.t_conv2(x5c)  # -> /4
+        x6c = torch.cat([x6, x2], axis=1)
+        x7 = self.t_conv3(x6c)  # -> /2
+        x7c = torch.cat([x7, x1], axis=1)
+        x8 = self.t_conv4(x7c)
 
-        x7 = F.leaky_relu(self.t_conv7(x6))
-        x7 = torch.cat([x7, x3], axis=1)
-        x7 = F.leaky_relu(self.conv7a(x7))
-        x7 = F.leaky_relu(self.conv7b(x7))
-
-        x8 = F.leaky_relu(self.t_conv8(x7))
-        x8 = torch.cat([x8, x2], axis=1)
-        x8 = F.leaky_relu(self.conv8a(x8))
-        x8 = F.leaky_relu(self.conv8b(x8))
-
-        x9 = F.leaky_relu(self.t_conv9(x8))
-        x9 = torch.cat([x9, x1], axis=1)
-        x9 = F.leaky_relu(self.conv9a(x9))
-        x9 = F.leaky_relu(self.conv9b(x9))
-
-        out = self.conv_out(x9)
+        out = self.conv_last(x8)
 
         return torch.tanh(out)
 
@@ -150,36 +156,31 @@ class Pix2PixDiscriminator(nn.Module):
 
         super(Pix2PixDiscriminator, self).__init__()
 
-        self.conv1 = nn.Conv2d(
+        self.conv1 = ConvBlock(
             in_channels=n_inp_channels, out_channels=n_filters,
-            kernel_size=5, stride=1, padding=2)
+            kernel_size=3, name='d1', stride=2, padding=1,
+            activation='leaky_relu', bn=False, dropout=False, transposed=False)
 
-        self.conv2 = nn.Conv2d(
+        self.conv2 = ConvBlock(
             in_channels=n_filters, out_channels=n_filters*2,
-            kernel_size=5, stride=2, padding=2)
+            kernel_size=3, name='d2', stride=2, padding=1,
+            activation='leaky_relu', bn=True, dropout=False, transposed=False)
 
-        self.conv3 = nn.Conv2d(
+        self.conv3 = ConvBlock(
             in_channels=n_filters*2, out_channels=n_filters*4,
-            kernel_size=5, stride=1, padding=2)
+            kernel_size=3, name='d3', stride=2, padding=1,
+            activation='leaky_relu', bn=True, dropout=False, transposed=False)
 
-        self.conv4 = nn.Conv2d(
+        self.conv4 = ConvBlock(
             in_channels=n_filters*4, out_channels=n_filters*8,
-            kernel_size=5, stride=2, padding=2)
-
-        self.conv5 = nn.Conv2d(
-            in_channels=n_filters*8, out_channels=1,
-            kernel_size=5, stride=1, padding=2)
+            kernel_size=3, name='d4', stride=2, padding=1,
+            activation='leaky_relu', bn=True, dropout=False, transposed=False)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = torch.relu(x)
         x = self.conv2(x)
-        x = torch.relu(x)
         x = self.conv3(x)
-        x = torch.relu(x)
         x = self.conv4(x)
-        x = torch.relu(x)
-        x = self.conv5(x)
         x = torch.sigmoid(x)
 
         return x
