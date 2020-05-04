0086d6ae6ecf12c5b2994195c553742c8f12f18b
Vahid MIRJALILI <@vmirly>
2020-05-03 22:44:08 -0500

    added Reshape layer

--- ops/torch_ops.py
+++ ops/torch_ops.py
@@ -2,5 +2,20 @@ import torch.nn as nn
 
 
 class Flatten(nn.Module):
+    """
+    A custom layer to flatten the feature maps
+    """
     def forward(self, input):
         return input.view(input.size(0), -1)
+
+
+class Reshape(nn.Module):
+    """
+    A custom layer for reshaping feature maps
+    """
+    def __init__(self, new_shape):
+        super(Reshape, self).__init__()
+        self.new_shape = new_shape
+
+    def forward(self, input):
+        return input.view(input.size(0), *self.new_shape)
