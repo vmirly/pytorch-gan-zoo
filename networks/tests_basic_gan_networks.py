474018ea7bd001500ccc9b1f0a0a734d0f94ff66
Vahid MIRJALILI <@vmirly>
2020-05-03 22:45:19 -0500

    unittest for basic-gan-networks

---
+++ networks/tests_basic_gan_networks.py
@@ -0,0 +1,33 @@
+import unittest
+import torch
+import numpy as np
+from networks import basic_gan_networks as nets
+
+
+class TestBasicGANNetworks(unittest.TestCase):
+
+    def test_fc_networks(self):
+        gen = nets.make_generator(10, 16, 28)
+        disc = nets.make_discriminator(28, 16)
+
+        z = np.random.normal(size=(1, 10)).astype(np.float32)
+        z = torch.tensor(z)
+
+        gen_output = gen(z)
+        self.assertTrue(np.array_equal(gen_output.shape, (1, 1, 28, 28)))
+
+        disc_output = disc(gen_output)
+        self.assertTrue(np.array_equal(disc_output.shape, (1, 1)))
+
+    def test_conv_networks(self):
+        gen = nets.make_conv_generator(10, 64)
+        disc = nets.make_conv_discriminator()
+
+        z = np.random.normal(size=(4, 10)).astype(np.float32)
+        z = torch.tensor(z)
+
+        gen_output = gen(z)
+        self.assertTrue(np.array_equal(gen_output.shape, (4, 1, 28, 28)))
+
+        disc_output = disc(gen_output)
+        self.assertTrue(np.array_equal(disc_output.shape, (4, 1)))
