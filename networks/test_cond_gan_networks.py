b2cb85193180751ffb61427ed54332159593e687
Vahid MIRJALILI <@vmirly>
2020-05-06 15:19:16 -0500

    Added a new test-function

--- networks/test_cond_gan_networks.py
+++ networks/test_cond_gan_networks.py
@@ -51,3 +51,26 @@ class TestCondGANNetworks(unittest.TestCase):
 
         disc_output = cond_gan.disc_forward(gen_output, c)
         self.assertTrue(np.array_equal(disc_output.shape, (4, 1)))
+
+    def test_conv_networks_with_fusion(self):
+
+        cond_gan = nets.ConditionalConvGAN(
+            num_z_units=10,
+            num_cond_vals=2,
+            image_dim=28,
+            image_channels=1,
+            g_num_filters=64,
+            d_num_filters=8,
+            use_fusion_layer=True)
+
+        z = np.random.normal(size=(4, 10)).astype(np.float32)
+        z = torch.tensor(z)
+
+        c = np.random.uniform(size=(4, 2)).astype(np.float32)
+        c = torch.tensor(c)
+
+        gen_output = cond_gan.gen_forward(z, c)
+        self.assertTrue(np.array_equal(gen_output.shape, (4, 1, 28, 28)))
+
+        disc_output = cond_gan.disc_forward(gen_output, c)
+        self.assertTrue(np.array_equal(disc_output.shape, (4, 1)))
