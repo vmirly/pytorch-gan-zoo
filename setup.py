c893417edd1508c0a1977c836566e7b3e5de5bb3
vmirly <mail@vahidmirjalili.com>
2022-11-08 23:03:43 -0600

    initial commit of package files

---
+++ setup.py
@@ -0,0 +1,41 @@
+import io
+import os.path as osp
+import sys
+from setuptools import setup, find_packages
+
+import ganzoo
+
+NAME = "pytorch-gan-zoo"
+DESCRIPTION = "Implementation of GAN models in PyTorch."
+URL = "https://github.com/vmirly/pytorch-gan-zoo"
+AUTHOR = "Vahid Mirjalili"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = ganzoo.__version__
+
+
+try:
+    with io.open("README.md") as fp:
+        LONG_DESCRIPTION = fp.read()
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
+
+EXTRAS = {
+    "test": [
+        "pytest",
+        "mock",
+        "flake8==4.0.1",
+        "flake8-docstrings==1.6.0"
+    ]
+}
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    maintainer=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(include=['ganzoo', 'ganzoo.*'])
+)
