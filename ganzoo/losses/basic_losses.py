22a68c85c04563b89a16013aa240310b0067e714
vmirly <mail@vahidmirjalili.com>
2022-11-10 20:09:48 -0600

    basic loss functions + tests

---
+++ ganzoo/losses/basic_losses.py
@@ -0,0 +1,36 @@
+"""
+PyTorch GAN Zoo
+Author: Vahid Mirjalili
+"""
+
+import torch
+import torch.nn.functional as F
+
+
+def basic_gan_lossfn_G(d_fake: torch.Tensor) -> torch.Tensor:
+    """
+    Basic GAN loss for the generator network
+    """
+    return F.binary_cross_entropy(
+        input=d_fake,
+        target=torch.ones_like(d_fake))
+
+
+def basic_gan_lossfn_D_real(d_real: torch.Tensor) -> torch.Tensor:
+    """
+    Basic GAN loss for the discriminator network
+    (real data)
+    """
+    return F.binary_cross_entropy(
+        input=d_real,
+        target=torch.ones_like(d_real))
+
+
+def basic_gan_lossfn_D_fake(d_fake: torch.Tensor) -> torch.Tensor:
+    """
+    Basic GAN loss for the discriminator network
+    (fake/synthesized data)
+    """
+    return F.binary_cross_entropy(
+        input=d_fake,
+        target=torch.zeros_like(d_fake))
---
+++ ganzoo/tests/test_losses_gan.py
@@ -0,0 +1,27 @@
+"""
+PyTorch GAN Zoo - unit-tests for basic-loss functions
+Author: Vahid Mirjalili
+"""
+
+import torch
+import numpy as np
+
+from ganzoo.losses import basic_losses
+
+
+def test_basic_gan_loss_G():
+    t = torch.tensor([1.0, 0.5, 1.0])
+    loss_g = basic_losses.basic_gan_lossfn_G(t)
+    assert np.isclose(loss_g, 0.2310, atol=1e-4)
+
+
+def test_basic_gan_loss_D_fake():
+    t = torch.tensor([0.1, 0.5, 0.1])
+    loss_d_fake = basic_losses.basic_gan_lossfn_D_fake(t)
+    assert np.isclose(loss_d_fake, 0.3013, atol=1e-4)
+
+
+def test_basic_gan_loss_D_real():
+    t = torch.tensor([1.0, 0.5, 1.0])
+    loss_d_real = basic_losses.basic_gan_lossfn_D_real(t)
+    assert np.isclose(loss_d_real, 0.2310, atol=1e-4)
