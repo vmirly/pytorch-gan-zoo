5e0b169b132313bec3796e0dd21f1c0f9de442cf
vmirly <mail@vahidmirjalili.com>
2022-11-21 00:07:00 -0600

    First version of training-script for DCGAN

---
+++ ganzoo/examples/dcgan/parsers.py
@@ -0,0 +1,40 @@
+
+"""
+PyTorch GAN Zoo
+Author: Vahid Mirjalili
+"""
+import argparse
+from ganzoo.constants import names
+from ganzoo.constants import defaults
+
+
+def parse_basicfc_train_opts(argv):
+    parser = argparse.ArgumentParser(__file__)
+    
+    parser.add_argument(
+        '--dataset_name', type=str, required=True,  # TODO: add custom-folder 
+        choices=names.DOWNLOADABLE_DATASETS + [names.NAMESTR_CELEBA])
+
+    parser.add_argument(
+        '--z_dim', type=int, required=False,
+        default=defaults.Z_DIM)
+
+    parser.add_argument(
+        '--z_distribution', type=str,
+        default=names.NAMESTR_UNIFORM,
+        choices=names.LIST_DISTRBUTIONS)
+
+    parser.add_argument(
+        '--num_conv_filters', type=int,
+        default=defaults.NUM_CONV_KERNELS)
+
+    parser.add_argument(
+        '--batch_size', type=int,
+        default=defaults.BATCH_SIZE)
+
+    parser.add_argument(
+        '--num_epochs', type=int, required=False,
+        default=defaults.NUM_EPOCHS)
+
+    args = parser.parse_args()
+    return args
---
+++ ganzoo/examples/dcgan/train.py
@@ -0,0 +1,67 @@
+"""
+PyTorch GAN Zoo -- script to train DCGAN using pytorch-lightning
+Author: Vahid Mirjalili
+"""
+
+import sys
+import logging
+import torch
+import pytorch_lightning as pl
+import torchvision
+from torchvision import transforms as T
+
+from ganzoo.lit_modules import dcgan
+from ganzoo.lit_modules import lit_data_vision
+from ganzoo.examples.dcgan import parsers
+from ganzoo.misc import utils
+
+
+def main(args):
+
+    image_shape, msg = utils.get_dataset_props(args.dataset_name)
+    if msg:
+        return msg
+    image_h, image_w, image_c = image_shape
+
+    if image_h != image_w:
+        if max([image_h, image_w]) > 32 and min([mage_h, image_w]) < 32:
+            return 'Unexpected data shape!'
+
+    image_dim = image_h
+    image_channels = image_c
+
+    model = dcgan.LitDCGAN(
+        num_z_units=args.z_dim,
+        z_distribution=args.z_distribution,
+        num_conv_filters=args.num_conv_filters,
+        image_dim=32, image_channels=image_channels,
+        lr=0.001, beta1=0.5, beta2=0.9
+    )
+
+    trsfm = T.Compose([
+        T.Resize(32),
+        T.CenterCrop(32),
+        T.ToTensor(),
+        T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+
+    dm = lit_data_vision.LitVisionDataset(
+        dataset_name=args.dataset_name,
+        transform=trsfm,
+        splits=(0.9, 0.1),
+        batch_size=args.batch_size)
+
+    trainer = pl.Trainer(
+        accelerator="auto",
+        devices=1 if torch.cuda.is_available() else None,
+        max_epochs=args.num_epochs,
+    )
+    trainer.fit(model, dm)
+
+
+if __name__ == '__main__':
+    args = parsers.parse_basicfc_train_opts(sys.argv[1:])
+    msg = main(args)
+    if msg:
+        logging.error(msg)
+        sys.exit(1)
