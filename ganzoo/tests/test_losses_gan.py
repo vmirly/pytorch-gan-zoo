98883429f260a5c420fecbe8258378b742ba139b
vmirly <mail@vahidmirjalili.com>
2022-11-23 10:56:25 -0600

    unittest for wgan-losses

--- ganzoo/tests/test_losses_gan.py
+++ ganzoo/tests/test_losses_gan.py
@@ -7,6 +7,7 @@ import torch
 import numpy as np
 
 from ganzoo.losses import basic_losses
+from ganzoo.losses import wgan_losses
 
 
 def test_vanilla_gan_loss_G():
@@ -25,3 +26,22 @@ def test_vanilla_gan_loss_D_real():
     t = torch.tensor([1.0, 0.5, 1.0])
     loss_d_real = basic_losses.vanilla_gan_lossfn_D_real(t)
     assert np.isclose(loss_d_real, 0.2310, atol=1e-4)
+
+
+def test_wgan_lossfn_G():
+    t = torch.tensor([1.2, -0.5, 2.5, -2.2])
+    loss_g = wgan_losses.wgan_lossfn_G(t)
+    assert np.isclose(loss_g, -0.25, atol=1e-4)
+
+
+def test_wgan_lossfn_D_real():
+    t = torch.tensor([1.2, -0.5, 2.5, -2.2])
+    loss_d_real = wgan_losses.wgan_lossfn_D_real(t)
+    assert np.isclose(loss_d_real, -0.25, atol=1e-4)
+
+
+def test_wgan_lossfn_D_fake():
+    t = torch.tensor([1.2, -0.5, 2.5, -2.2])
+    loss_d_fake = wgan_losses.wgan_lossfn_D_fake(t)
+    assert np.isclose(loss_d_fake, 0.25, atol=1e-4)
+
