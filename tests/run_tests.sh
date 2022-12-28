e51d96c6e1886477c2f4fd27c455c81299155d5e
Vahid Mirjalili <mail@vahidmirjalili.com>
2022-12-27 21:14:54 -0600

    Extended bash-test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,3 +9,13 @@ for loss in vanilla wgan wgan-gp wgan-lp ; do
 	--dataset_name=fashion-mnist \
 	--loss_type=${loss};
 done
+
+
+# Running DCGAN
+for loss in vanilla wgan wgan-gp wgan-lp ; do
+    python -m run.basic_gan.train_dcgan \
+        --num_epochs=5  --z_dim=100 \
+	--num_conv_filters=32  \
+	--dataset_name=fashion-mnist \
+	--loss_type=${loss};
+done
